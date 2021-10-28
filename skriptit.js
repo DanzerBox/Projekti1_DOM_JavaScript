7bf48308bc4ef0d43320c331307c28488b9c9eb2
DanzerBox <danzerbox@gmail.com>
2021-10-28 07:47:50 +0300

    m

--- skriptit.js
+++ skriptit.js
@@ -1,57 +1,97 @@
-//<script>
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+window.onload = () => {
+  const form1 = document.querySelector("#addForm");
+
+  let items = document.getElementById("items");
+  let submit = document.getElementById("submit");
+
+  let editItem = null;
+
+  form1.addEventListener("submit", addItem);
+  items.addEventListener("click", removeItem);
+};
+
+function addItem(e) {
+  e.preventDefault();
+
+  if (submit.value != "Talleta") {
+      console.log("Modataan tehtävää");
+
+      editItem.target.parentNode.childNodes[0].data
+          = document.getElementById("item").value;
+
+      submit.value = "Submit";
+      document.getElementById("item").value = "";
+
+      document.getElementById("lblsuccess").innerHTML
+          = "Textin päivitys onnistui";
+
+      document.getElementById("lblsuccess")
+          .style.display = "block";
+
+      setTimeout(function () {
+          document.getElementById("lblsuccess")
+              .style.display = "none";
+      }, 3000);
+
+      return false;
+  }
+
+  let newItem = document.getElementById("item").value;
+  if (newItem.trim() == "" || newItem.trim() == null)
+      return false;
+  else
+      document.getElementById("item").value = "";
+
+  let li = document.createElement("li");
+  li.className = "list-group-item";
+
+  let deleteButton = document.createElement("button");
+
+  deleteButton.className =
+      "btn-danger btn btn-sm float-right delete";
+
+  deleteButton.appendChild(document.createTextNode("Poista"));
+
+  let editButton = document.createElement("button");
+
+  editButton.className =
+      "btn-success btn btn-sm float-right edit";
+
+  editButton.appendChild(document.createTextNode("Muokkaa"));
+
+  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(deleteButton);
+  li.appendChild(editButton);
+
+  items.appendChild(li);
 }
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
+function removeItem(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("delete")) {
+      if (confirm("Are you Sure?")) {
+          let li = e.target.parentNode;
+          items.removeChild(li);
+          document.getElementById("lblsuccess").innerHTML
+              = "Textin poistaminen onnistui";
+
+          document.getElementById("lblsuccess")
+              .style.display = "block";
+
+          setTimeout(function () {
+              document.getElementById("lblsuccess")
+                  .style.display = "none";
+          }, 3000);
+      }
+  }
+  if (e.target.classList.contains("edit")) {
+      document.getElementById("item").value =
+          e.target.parentNode.childNodes[0].data;
+      submit.value = "EDIT";
+      editItem = e;
   }
 }
 
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+function toggleButton(ref, btnID) {
+  document.getElementById(btnID).disabled = false;
 }
-//</script>
